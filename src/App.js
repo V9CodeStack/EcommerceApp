397c33010f4221ec2c0130fb2ba643446d7a2bbe
V9CodeStack <v9codestack@gmail.com>
2023-09-22 17:45:30 +0530

    second

--- src/App.js
+++ src/App.js
@@ -15,9 +15,11 @@ import "./App.css";
 class App extends Component {
   state = {
     cartList: [],
-  };
 
-  //   TODO: Add your code for remove all cart items, increment cart item quantity, decrement cart item quantity, remove cart item
+    //vinay
+  };
+  // print
+  //   TODO: Add your code for remove all cart items, increment cart item quantity, decrement cart item quantity, remove cart item  print
   removeAllCartItems = () => {
     this.setState({ cartList: [] });
   };
